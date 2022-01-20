53eb4cc0be44a9e7ac7a87a19430d18a7ffbe583
Carlos Cenjor Ramirez <carlos.cenjor@sngular.com>
2022-01-21 00:37:57 +0100

    trying solutions

--- pipelines/common-steps/deploy.sh
+++ pipelines/common-steps/deploy.sh
@@ -2,8 +2,8 @@
 
 set -x
 du -hs * | sort -h
-echo $AWS_ACCESS_KEY_ID
-echo $AWS_SECRET_ACCESS_KEY
-echo $AWS_SESSION_TOKEN
-echo $AWS_DEFAULT_REGION
+#echo $AWS_ACCESS_KEY_ID
+#echo $AWS_SECRET_ACCESS_KEY
+#echo $AWS_SESSION_TOKEN
+#echo $AWS_DEFAULT_REGION
 sam deploy template.yaml --config-env ${ENVIRONMENT} --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar
